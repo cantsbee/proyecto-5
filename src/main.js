85e60fa97d64415a99d397080d8508eaf39c2323
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-08-09 14:12:15 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -165,7 +165,7 @@ async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/");
+    params.append("redirect_uri", "https://proyecto-5-iota.vercel.app/");
     params.append("scope", "user-read-private user-read-email user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
