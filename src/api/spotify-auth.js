447c7b75e280accf301d8bc5a1117f9e8cbe5f83
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-08-09 14:04:13 +0200

    Update spotify-auth.js

--- src/api/spotify-auth.js
+++ src/api/spotify-auth.js
@@ -1,4 +1,4 @@
-const clientId = "6e4ca8910c3c479ea21b9de20ca7646c"; 
+const clientId = "da6c869ff792411388e5dce6c6032054"; 
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
@@ -12,7 +12,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:5173/"); // Ajustado para desarrollo local
+  params.append("redirect_uri", "https://proyecto-5-iota.vercel.app/"); 
   params.append("scope", "user-read-private user-read-email user-top-read");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -47,7 +47,7 @@ export async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", " ")// Ajustado para desarrollo local
+  params.append("redirect_uri", "https://proyecto-5-iota.vercel.app/");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
